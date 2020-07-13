5f9d51237276b71d7de94d5a5568a4ec69a1f110
odyssic <stevenbuddenjr@gmail.com>
2020-07-13 16:02:21 -0400

    one card now, centered

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import WeatherEngine from "./components/WeatherEngine";
 
 function App() {
   return (
-    <div className="App">
-      <WeatherEngine location="Sydney, au" />
-      <WeatherEngine location="Phoenix, us" />
+    <div className="app-container">
+      <div className="App">
+        <WeatherEngine location="Sydney, au" />
+        {/* <WeatherEngine location="Phoenix, us" /> */}
+      </div>
     </div>
   );
 }
